703434ad90237099db3109af3e34e1fb37edf671
ynam-fo-eno <1202dtim@gmail.com>
2024-10-26 09:46:19 +0300

    Ops

--- src/main.zig
+++ src/main.zig
@@ -1,90 +1,62 @@
 const std: type = @import("std");
 
-//Variable names are generally in snake case as you'll observe below.
-
-//This one will be unchanged throughout program's execution...
-const global_const: u8 = 18;
-
-//..but this one is variable despite having the same initial value
-var global_var: u8 = 18;
-
-//Function names, btw, are camel case.
-
-fn printStuff(name: []const u8, x: anytype) void {
-    std.debug.print("{s: >10} {any: ^10}\t{}\n", .{ name, x, @TypeOf(x) });
-}
-
 pub fn main() !void {
-    //Lines for labelling output apparently
-    std.debug.print("{s: >10} {s: ^10}\t{s}\n", .{ "name", "value", "type" });
-    std.debug.print("{s: >10} {s: ^10}\t{s}\n", .{ "___", "___", "___" });
+    const zero: u8 = 0;
+    const one: u8 = 1;
+    const two: u8 = 2;
+    const two_fifty: u8 = 250;
 
-    //More of const: immutable values here.
-    const a_const = 1;
-    //Just as I expected- can't modify const hivi hivi- it's literally in the name!!
-    //a_const+=1;
-    printStuff("a_const", a_const);
+    //below is fine, should turn to 252...
+    var result = (one * zero) + (two + two_fifty);
+    std.debug.print("Result = {}\n", .{result});
+    //...but this isn't and yields buffer overflow- unless you use % for wrapping!
+    result = (two *% two_fifty);
+    std.debug.print("*% Result = {}\n", .{result});
+    //Use single | for saturation
+    result = (two *| two_fifty);
+    std.debug.print("*| Result = {}\n", .{result});
+    //This yields something smaller than zero (another potential case of overflow)
+    //hence our use of wrapping to get this to 255 also
+    result = zero -% one;
+    std.debug.print("-% Result = {}\n", .{result});
+    //Same idea except sasa we use | to saturate and limit it to zero.
+    result = zero -| one;
+    std.debug.print("-| Result = {}\n", .{result});
 
-    //More of var: mutable values here.
-    comptime var a_var = 1;
-    //Unsurprisingly, we can modify this as usual.
-    a_var += 1;
-    printStuff("a_var", a_var);
+    //Shifting ops
+    _ = 1 << 2;
+    _ = 1 <<| 2;
+    _ = 32 >> 1;
 
-    //Note that both need you to initialize the variables,
-    //and give them a type or give them one that can be
-    //inferred at compile time.
+    //Bitwise logical ops
+    _ = 32 | 2; //or
+    _ = 32 & 1; //and
+    _ = 32 ^ 0; //xor
+    const one_bit: u8 = 0b0000_0001;
+    _ = ~one_bit; //not
 
-    comptime var b_var = 12;
-    b_var *= 2;
-    printStuff("b_var", b_var);
-    //To try to make b_var as commented immediately below would yield an error:
-    // var b_var =12;
+    //Comparison ops
+    _ = 3 < 9;
+    _ = 3 <= 9;
+    _ = 3 > 9;
+    _ = 3 >= 9;
+    _ = 3 == 9;
+    _ = 3 != 9;
 
-    //Leaving variables uninitialized needs to be defined safely by using "undfefined"
-    var c_var: u8 = undefined;
-    printStuff("c_var(before)", c_var);
-    //Prints nonsense value, but...
-    c_var = 12;
-    printStuff("c_var(after)", c_var);
+    //How to do type coerciion safely in Zig (more on this later)...
+    const uno: u8 = 200;
+    const dos: u16 = 999;
+    const tres: u32 = (uno + dos);
+    std.debug.print("Tres: {}\n", .{tres});
+    const quatro: u16 = @intCast(tres);
+    std.debug.print("Quatro : {}\n", .{quatro});
 
-    //Notice how Zig doesn't permit you to make a variable and then never use it.
-    //Indeed this program imekuwa ikiwika until I either increment the variable
-    //and/or push it to that printing function we made. The only way to sort of avoid
-    //using a variable is assigning your variable to an underscore as shown below.
-    const b_const: u8 = 13;
-    _ = b_const;
+    const a_float: f32 = 3.14195;
+    const an_int: i32 = @intFromFloat(a_float);
+    std.debug.print("Pi (as usual) : {}\n", .{a_float});
+    std.debug.print("Pi (as an int) : {}\n", .{an_int});
 
-    //Generally variables include:
-    // 1. Integers
-    //      Unsigned (+): u8 || u16 || u32 || u64 || u128 || usize
-    //      Signed (+ && -): i8 || i16 || i32 || i64 || i128 || isize
-    // 2. Literals
-    //      Can be Decimal ||  Binary || Octal || Hexadecimal
-    //You may use underscores for big values for #1 and #2 to make
-    //said values more readable.
-
-    //Usize denotes you can use any number of bytes you'd prefer
-    //so long as you aren't out of range in the end of assignment.
-    //Like below, I can assign 1 or 0 but not 2 onwards.
-    var d_var: u1 = 0;
-    d_var = 1;
-    printStuff("d_var", d_var);
-
-    //Now to floating kiasi, we can use e or E for exponentiation as we shall see
-
-    var e_var: f64 = 2_000_000E-6;
-    e_var += 1_000_000;
-    printStuff("e_var", e_var);
-
-    var f_var: f64 = 1_000_000e-6;
-    f_var += 1_000_000;
-    printStuff("f_var", f_var);
-
-    const c_const = 3.1415;
-    printStuff("c_const", c_const);
-    //There's ways to denote infinity or undefined numbers as shown below
-    //_ = std.math.inf(f64);
-    // _ = -(std.math.inf(f64));
-    // _ = std.math.nan(f64);
+    //Note Zig has std.math for other ops.
+    //For your personal further exploration.
+    //Other useful ops include : @addWithOverflow, @mod, @rem, @sqrt, @max, @min,...
 }
